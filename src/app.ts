a91aaf8cf43a148c8ee8a12edca3eaa38f0b981c
rafiqmia65 <rafiqdeveloper4465@gmail.com>
2026-03-28 16:35:28 +0600

    feat: complete initial setup with TypeScript, Prisma ORM, and PostgreSQL database integration

---
+++ src/app.ts
@@ -0,0 +1,44 @@
+import cors from "cors";
+import express, {
+  type Application,
+  type Request,
+  type Response,
+} from "express";
+
+const app: Application = express();
+
+// app.use(
+//   cors({
+//     origin: [
+//       envVars.FRONTEND_URL,
+//       envVars.BETTER_AUTH_URL,
+//       "http://localhost:3000",
+//       "http://localhost:5000",
+//     ],
+//     credentials: true,
+//     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+//     allowedHeaders: ["Content-Type", "Authorization"],
+//   }),
+// );
+
+// app.use("/api/auth", toNodeHandler(auth));
+
+// Enable URL-encoded form data parsing
+app.use(express.urlencoded({ extended: true }));
+
+// Middleware to parse JSON bodies
+app.use(express.json());
+// app.use(cookieParser());
+app.use(express.urlencoded({ extended: true }));
+
+// app.use("/api/v1", indexRoutes);
+
+// Basic route
+app.get("/", async (req: Request, res: Response) => {
+  res.status(201).json({
+    success: true,
+    message: "API is working",
+  });
+});
+
+export default app;
