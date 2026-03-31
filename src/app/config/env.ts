711759402ddb870d490ab8b8dc0e17c2eca7ca09
rafiqmia65 <rafiqdeveloper4465@gmail.com>
2026-03-31 18:02:52 +0600

    chore: add User model additional fields (role, status, needPasswordChange, isDeleted, deletedAt, image, emailVerified) to BetterAuth config

--- src/app/config/env.ts
+++ src/app/config/env.ts
@@ -7,8 +7,8 @@ interface EnvConfig {
   PORT: string;
   NODE_ENV: string;
   DATABASE_URL: string;
-  //   BETTER_AUTH_URL: string;
-  //   BETTER_AUTH_SECRET: string;
+    BETTER_AUTH_URL: string;
+    BETTER_AUTH_SECRET: string;
   //   ACCESS_TOKEN_SECRET: string;
   //   REFRESH_TOKEN_SECRET: string;
   //   ACCESS_TOKEN_EXPIRES_IN: string;
@@ -44,8 +44,8 @@ const loadEnvVariables = (): EnvConfig => {
     "PORT",
     "NODE_ENV",
     "DATABASE_URL",
-    // "BETTER_AUTH_URL",
-    // "BETTER_AUTH_SECRET",
+    "BETTER_AUTH_URL",
+    "BETTER_AUTH_SECRET",
     // "ACCESS_TOKEN_SECRET",
     // "REFRESH_TOKEN_SECRET",
     // "ACCESS_TOKEN_EXPIRES_IN",
@@ -83,8 +83,8 @@ const loadEnvVariables = (): EnvConfig => {
     PORT: process.env.PORT as string,
     NODE_ENV: process.env.NODE_ENV as string,
     DATABASE_URL: process.env.DATABASE_URL as string,
-    // BETTER_AUTH_URL: process.env.BETTER_AUTH_URL as string,
-    // BETTER_AUTH_SECRET: process.env.BETTER_AUTH_SECRET as string,
+    BETTER_AUTH_URL: process.env.BETTER_AUTH_URL as string,
+    BETTER_AUTH_SECRET: process.env.BETTER_AUTH_SECRET as string,
     // ACCESS_TOKEN_SECRET: process.env.ACCESS_TOKEN_SECRET as string,
     // REFRESH_TOKEN_SECRET: process.env.REFRESH_TOKEN_SECRET as string,
     // ACCESS_TOKEN_EXPIRES_IN: process.env.ACCESS_TOKEN_EXPIRES_IN as string,
--- src/app/lib/auth.ts
+++ src/app/lib/auth.ts
@@ -1,12 +1,55 @@
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import { prisma } from "./prisma";
+import { envVars } from "../config/env";
+import { Role, UserStatus } from "../../../generated/prisma/enums";
 
 export const auth = betterAuth({
+  baseURL: envVars.BETTER_AUTH_URL,
+  secret: envVars.BETTER_AUTH_SECRET,
   database: prismaAdapter(prisma, {
     provider: "postgresql", // or "mysql", "postgresql", ...etc
   }),
   emailAndPassword: {
     enabled: true,
+  },
+  user: {
+    additionalFields: {
+      role: {
+        type: "string",
+        required: true,
+        defaultValue: Role.MEMBER,
+      },
+      status: {
+        type: "string",
+        required: true,
+        defaultValue: UserStatus.ACTIVE,
+      },
+      needPasswordChange: {
+        type: "boolean",
+        required: true,
+        defaultValue: false,
+      },
+      isDeleted: {
+        type: "boolean",
+        required: true,
+        defaultValue: false,
+      },
+      deletedAt: {
+        type: "date",
+        required: false,
+        defaultValue: null,
+      },
+      image: {
+        type: "string",
+        required: false,
+        defaultValue: null,
+      },
+      emailVerified: {
+        type: "boolean",
+        required: true,
+        defaultValue: false,
+      },
+    },
   },
 });
